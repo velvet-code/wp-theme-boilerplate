82d0bf15b661cc52e2fa7e3f9f2405a12f8507ed
RaunoT <raunoot@gmail.com>
2020-07-09 14:20:34 +0300

    Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [
+    './**/*.html',
+    './**/*.php',
+    './src/**/*.css',
+    './src/**/*.js'
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        black: '#000',
+        white: '#fff'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
